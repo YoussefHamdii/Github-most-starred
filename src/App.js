bcd1ca7cae4213ddb4bd7822548a37bd1ee70e7d
Youssef Hamdi <youssefhamdih11@gmail.com>
2021-07-31 23:09:56 +0200

    Data fetched/creating repo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Repo from './components/Repo';
 
 function App() {
+  // data retrieved from the api as array to use map function
+  const [data, setData] = useState([]);
+
+  // page number for pagination
+  const [page, setPage] = useState(1);
+
+  // async function to get the repos
+  async function fetchData(){
+    const data = await axios.get(`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${page}`);
+    setData(data.data.items);
+  }
+  
+  // initializing data from api before rendering
+  useEffect(()=>fetchData(),[]);
+
+  console.log(data);
   return (
     <div className="App">
-      
+      <Repo />
     </div>
   );
 }
---
+++ src/components/Repo.js
@@ -0,0 +1,20 @@
+
+
+function Repo (){
+    return(
+        <div className="repo__container">
+            <img alt="Avatar" src=""/>
+            <div className="repo__body">
+                <h5>Hello</h5>
+                <p>hello helloh kjASgflsdGfsdgfkbsdjfsdfib</p>
+                <div className="repo__status">
+                    <span>stars</span>
+                    <span>issues</span>
+                    <span>120-1213-1133</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Repo;
